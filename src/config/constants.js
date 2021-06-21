92d401647210d1fb534552cee43976daf98ec2d8
Josse Saenz <josesaenz095@gmail.com>
2021-06-21 15:27:46 -0400

    First Commit

---
+++ src/config/constants.js
@@ -0,0 +1,1 @@
+export const GIPHY_API_KEY = 'unymfHDDv5fRmTkYtbfh8RfKXTbwbpKt';
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+import { GIPHY_API_KEY } from "../config/constants";
+
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${GIPHY_API_KEY}`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    return gifs;
+}
